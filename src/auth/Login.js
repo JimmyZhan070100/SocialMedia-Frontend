0cc4a85fc0a66628b40e48ed4a97f1bbbbac66f8
Jimmycofer <jimmy000701@gmail.com>
2023-12-02 18:57:20 -0600

    Integrate with Google login

--- src/auth/Login.js
+++ src/auth/Login.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "../styles/style.css";
 import { Link, useNavigate } from "react-router-dom";
 import { connect, useDispatch, useSelector } from "react-redux";
@@ -14,8 +14,55 @@ const Login = ({ updateForm }) => {
   const error = useSelector((state) => state.formData.error);
 
   const GoogleLogin = () => {
-    window.location.href = `${process.env.REACT_APP_BACKEND_URL}/auth/google`;
+    window.location.href = `${process.env.REACT_APP_BACKEND_URL}/auth/google/login`;
   };
+
+  // Function to retrieve and store user data
+  const retrieveAndStoreUserData = (username) => {
+    localStorage.setItem("user", JSON.stringify({ username: username }));
+    Promise.all([
+      dataFetchers.fetchEmail(username),
+      dataFetchers.fetchPhone(username),
+      dataFetchers.fetchZipcode(username),
+      dataFetchers.fetchDob(username),
+    ])
+      .then(([emailData, phoneData, zipcodeData, dobData]) => {
+        // Update the form data
+        const formDataUpdate = {
+          userName: username,
+          email: emailData.email,
+          phone: phoneData.phone,
+          zip: zipcodeData.zipcode,
+          birthDate: dobData.dob,
+          password: password,
+          confirmPassword: password,
+        };
+        updateForm(formDataUpdate);
+        navigate("/main");
+      })
+      .catch((error) => {
+        dispatch(loginFail(error.message));
+      });
+  };
+
+  useEffect(() => {
+    // Check if the URL contains a query parameter indicating a Google ID error
+    const queryParams = new URLSearchParams(window.location.search);
+    if (queryParams.has("googleError")) {
+      // Display the error message
+      const errorMessage = queryParams.get("message");
+      dispatch(loginFail(errorMessage));
+    }
+
+    // Check if the URL contains a query parameter indicating a successful Google login
+    if (queryParams.has("googleSuccess")) {
+      // Retrieve the username from the query parameter
+      const usernameFromGoogle = queryParams.get("username");
+
+      // Call the function to retrieve and store user data
+      retrieveAndStoreUserData(usernameFromGoogle);
+    }
+  }, []);
 
   const handleLogin = () => {
     fetch(`${process.env.REACT_APP_BACKEND_URL}/login`, {
